78f152e975ccc9b283909e39afbb91d9447450c2
seanbutcher1994 <110334815+seanbutcher1994@users.noreply.github.com>
2022-08-27 14:26:33 +0800

    Add Pseudocode and empty function called generatePassword

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+
+//Need to write a function called generatePassword to be able to create the randomised password with a set of criteria.
+function generatePassword() {
+  //Ask User How Long Is the Password
+
+  //Ask if I want to include
+  //UpperCase
+  //LowerCase
+  //Number
+  //Special Symbols
+
+  //Generate Password based on the criterias selected and the length
+
+  //1. Create the charset based on the criteria
+
+  //2. Loop for the 'length' times for each iteration
+  //grab a random char and append to an accumulatoer
+
+
+
+  //Pool of Characters
+
+  //Password is set of random characters based on the above pool
+
+
+}
 
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
